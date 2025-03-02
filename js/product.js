ab819309aec219a73cfb27452d667c319fa73773
Khadka <120006828+khadka27@users.noreply.github.com>
2025-03-02 23:17:38 +0545

    project Completed

---
+++ js/product.js
@@ -0,0 +1,13 @@
+//this is a alert function which displays some texts when add to cart is clicked
+
+function myFunction() {
+    var newLine = "\r\n";
+    var msg = "successfully add to cart";
+    msg += newLine;
+    msg += "Thankyou for Buying❤️";
+    msg += newLine;
+    msg += "Thankyou for visiting ";
+    msg += newLine;
+    msg += "Visit Again";
+    alert(msg);
+}
